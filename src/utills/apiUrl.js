b639723821687e8d51089f5d092c2849bcd14bdf
Park <qkrwns982@gmail.com>
2024-11-15 11:44:23 +0900

    docker again

--- src/utills/apiUrl.js
+++ src/utills/apiUrl.js
@@ -1,5 +1,5 @@
 const rootPath = "http://localhost:8000";
-// const rootPath = """
+// const rootPath = ""
 
 const GET_TASKS_API_URL = `${rootPath}/getTasks`;
 const DELETE_TASK_API_URL = `${rootPath}/deleteTask`;
